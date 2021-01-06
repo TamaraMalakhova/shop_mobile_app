c2c3295cc52c4342ebdcfa78afdb8ba49b0c3a9d
Tamara M <malahova-tamara@mail.ru>
2021-01-06 16:22:01 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+export default function App () {
+  return (
+    <View>
+      <Text>A new app!</Text>
+    </View>
+  );
+};
+
