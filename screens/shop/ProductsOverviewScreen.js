18da8b2a9982616957cba217064ffa26d88b0896
Tamara M <malahova-tamara@mail.ru>
2021-02-08 13:28:02 +0200

    Icons and cart-state were added

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,20 +1,51 @@
 import React from 'react';
-import { FlatList, Text } from 'react-native';
-import { useSelector } from 'react-redux';
+import { FlatList, Platform } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import ProductItem from '../../components/shop/ProductItem';
+import *  as cartActions from '../../store/actions/cart';
+import HeaderButton from '../../components/UI/HeaderButton';
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
+  const dispatch = useDispatch();
+
   return (
     <FlatList
       data={products}
       keyExtractor={item => item.data}
-      renderItem={itemData => <Text>{itemData.item.title}</Text>} 
+      renderItem={itemData => (
+        <ProductItem 
+          image={itemData.item.imageUrl} 
+          title={itemData.item.title} 
+          price={itemData.item.price} 
+          onViewDetail={() => {
+            props.navigation.navigate('ProductDetail', { 
+              productId: itemData.item.id,
+              productTitle: itemData.item.title 
+            });
+          }}
+          onAddToCart={() => {
+            dispatch(cartActions.addToCart(itemData.item));
+          }}
+          key={itemData.item.id}
+        />
+        )
+      } 
     />
   );
 };
 
 ProductsOverviewScreen.navigationOptions = {
-  headerTitle: 'All products'
+  headerTitle: 'All products',
+  headerRight: <HeaderButtons HeaderButtonComponent={HeaderButton}>
+    <Item 
+      title='Cart' 
+      iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart' }
+      onPress={() => {}}
+    />
+  </HeaderButtons>
 };
 
 export default ProductsOverviewScreen;
