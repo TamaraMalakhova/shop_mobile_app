0fc50b6d4d99569507d7ba362da3446530194dd7
Tamara M <malahova-tamara@mail.ru>
2021-03-21 10:39:08 +0200

    Firebase was added

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import ReduxThunk from 'redux-thunk';
 
 import productsReducer from './store/reducers/products';
 import cartReducer from './store/reducers/cart';
@@ -14,7 +14,7 @@ const rootReducer = combineReducers({
   orders: ordersReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App () {
   return (
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,16 +1,48 @@
-import React from 'react';
-import { FlatList, Platform, Button } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, Text, FlatList, Platform, Button, ActivityIndicator, StyleSheet } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import ProductItem from '../../components/shop/ProductItem';
 import *  as cartActions from '../../store/actions/cart';
+import * as productActions from '../../store/actions/products';
 import HeaderButton from '../../components/UI/HeaderButton';
 import Colors from '../../constants/Colors';
 
 const ProductsOverviewScreen = props => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRefreshing, setIsRefreshong] = useState(false);
+  const [error, setError] = useState();
   const products = useSelector(state => state.products.availableProducts);
   const dispatch = useDispatch();
+
+  const loadProducts = useCallback( async () => {
+    setError(null);
+    //setIsLoading(true);
+    setIsRefreshong(true);
+    try {
+      await dispatch(productActions.fetchProducts());
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsRefreshong(false);
+    //setIsLoading(false);
+  }, [dispatch, setIsLoading, setError]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    loadProducts().then(() => {
+      setIsLoading(false);
+    });
+  }, [dispatch, loadProducts]);
+
+  useEffect(() => {
+    const willFocusSub = props.navigation.addListener('willFocus', loadProducts);
+
+    return () => {
+      willFocusSub.remove();
+    };
+  }, [loadProducts]);
 
   const selectItemHandler = (id, title) => () => {
     props.navigation.navigate('ProductDetail', { 
@@ -19,8 +51,46 @@ const ProductsOverviewScreen = props => {
     });
   };
 
+  if (error) {
+    return (
+      <View style={styles.centered}>
+        <Text>
+          An error ocurred!
+        </Text>
+        <Button 
+          title='Try again'
+          onPress={loadProducts}
+          color={Colors.primary}
+        />
+      </View>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator
+          size='large'
+          color={Colors.primary} 
+        />
+      </View>
+    );
+  }
+
+  if (!isLoading && products.length === 0) {
+    return (
+      <View style={styles.centered}>
+        <Text>
+          No products found. Maybe start adding some!
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <FlatList
+      onRefresh={loadProducts}
+      refreshing={isRefreshing}
       data={products}
       keyExtractor={item => item.data}
       renderItem={itemData => (
@@ -73,5 +143,13 @@ ProductsOverviewScreen.navigationOptions = navData => {
     </HeaderButtons>
   };
 };
+
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center',
+  }
+});
 
 export default ProductsOverviewScreen;
